95173fd868cfe095bfaa033749ec9acac5d47b18
didrikhegna <didrik@snowball.digital>
2024-09-23 08:54:32 +0200

    prod endpoint

--- codegen.ts
+++ codegen.ts
@@ -1,18 +1,18 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-	schema: "https://api-dev.crystallize.digital/furnitut/discovery",
-	documents: ["app/**/*.graphql"],
-	ignoreNoDocuments: true,
-	overwrite: true,
+  schema: "https://api.crystallize.com/furnitut/discovery",
+  documents: ["app/**/*.graphql"],
+  ignoreNoDocuments: true,
+  overwrite: true,
   config: {
     avoidOptionals: true,
   },
-	generates: {
-		"generated/": {
-			preset: "client",
-		},
-	},
+  generates: {
+    "generated/": {
+      preset: "client",
+    },
+  },
 };
 
 export default config;
