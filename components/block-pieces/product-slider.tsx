4204539c3f8e17c775ca9e16f73cd7c3994aec20
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2025-01-30 12:42:09 +0200

    Fix product slider (#18)

--- components/block-pieces/product-slider.tsx
+++ components/block-pieces/product-slider.tsx
@@ -13,23 +13,25 @@ export const ProductSlider = ({ block }: ProductSliderProps) => {
     return (
         <div className="flex-col flex items-center py-12">
             <Typography {...block} />
-            {!!products?.variants && (
+            {!!products?.variants?.length && (
                 <div className="w-full pb-24">
                     <Slider options={{ loop: true, align: 'start' }}>
-                        {products.variants.map((item: any) => (
-                            <Product
-                                key={item.product.path}
-                                product={{
-                                    name: item.name,
-                                    path: item.product.path,
-                                    variants: null,
-                                    defaultVariant: {
-                                        firstImage: item.firstImage,
-                                        defaultPrice: item.defaultPrice,
-                                    },
-                                }}
-                            />
-                        ))}
+                        {products.variants.map((item) =>
+                            !!item?.product ? (
+                                <Product
+                                    key={item.product.path}
+                                    product={{
+                                        name: item.name,
+                                        path: item.product.path,
+                                        variants: null,
+                                        defaultVariant: {
+                                            firstImage: item.firstImage,
+                                            defaultPrice: item.defaultPrice,
+                                        },
+                                    }}
+                                />
+                            ) : null,
+                        )}
                     </Slider>
                 </div>
             )}
