dd2b03c3b8faf6816cedda03437f11d17c2df5ff
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2025-01-30 15:08:17 +0200

    Remove unused route (#20)

--- app/webhooks/[stripe]/route.ts
+++
@@ -1,8 +0,0 @@
-import { stripeWebhookHandler } from '@/use-cases/payments/stripe';
-import { NextResponse } from 'next/server';
-
-export async function POST(request: Request) {
-    const event = await request.text();
-    const data = await stripeWebhookHandler(request.headers.get('stripe-signature') as string, event);
-    return NextResponse.json({ data });
-}
