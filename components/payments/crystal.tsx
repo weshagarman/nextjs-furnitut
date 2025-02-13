a3dfe8a49d08c798dc1f73481dc7e69d3e3233d5
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2025-02-13 15:04:17 +0200

    feat: add cli template (#22)
    * Clean unused variable
    
    * Base setup
    
    * Add 2 products and folders
    
    * fix(install): make sure the modeling file is valid
    
    * Add story
    
    * Add home page
    
    ---------
    
    Co-authored-by: Morel Sébastien <morel.seb@gmail.com>

--- components/payments/crystal.tsx
+++ components/payments/crystal.tsx
@@ -2,7 +2,7 @@ import { makeCrystalPayment } from '@/app/actions/make-crystal-payment';
 import { useActionState } from 'react';
 
 export const Crystal = ({ cartId }: { cartId?: string }) => {
-    const [da, onSubmit, isPending] = useActionState(makeCrystalPayment, null);
+    const [, onSubmit, isPending] = useActionState(makeCrystalPayment, null);
 
     return (
         <div className="bg-soft mx-8 mt-4 rounded-lg px-6 py-4 flex gap-8 items-center ">
